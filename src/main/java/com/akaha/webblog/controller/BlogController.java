1abda183695d41a93ade2793c5b912902d5abb1e
Akaha LoKs <132211327+akaha-loks@users.noreply.github.com>
2025-11-17 20:36:20 +0600

    Added POST require for create posts

--- src/main/java/com/akaha/webblog/controller/BlogController.java
+++ src/main/java/com/akaha/webblog/controller/BlogController.java
@@ -4,6 +4,8 @@ import com.akaha.webblog.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -17,5 +19,16 @@ public class BlogController {
     public String blogMain(Model model){
         model.addAttribute("posts", postService.getPosts());
         return "blog-main";
+    }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model){
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model){
+        postService.addPost(title, anons, full_text);
+        return "redirect:/blog";
     }
 }
--- src/main/java/com/akaha/webblog/service/PostService.java
+++ src/main/java/com/akaha/webblog/service/PostService.java
@@ -14,4 +14,15 @@ public class PostService {
     public Iterable<Post> getPosts(){
         return postRepository.findAll();
     }
+
+    public Post addPost(String title, String anons, String full_text){
+        Post newPost = new Post(title, anons, full_text);
+        if (newPost.getTitle() == null || newPost.getTitle().isEmpty()) {
+            throw new IllegalArgumentException("Пустое название!");
+        }
+        if(newPost.getFull_text() == null || newPost.getFull_text().isEmpty()){
+            throw new IllegalArgumentException(newPost.getTitle() + ": не может быть пустым");
+        }
+        return postRepository.save(newPost);
+    }
 }
